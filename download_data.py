0cde1b9a36e8986f58786012cf89aa84dbc8a779
yuenhern.yu@gmail.com <yuenhern.yu@gmail.com>
2024-04-11 10:15:05 +0800

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 import subprocess
 
@@ -5,10 +6,15 @@ import subprocess
 ROOT_PATH = Path(__file__).resolve().parent
 DATA_PATH = ROOT_PATH / "data"
 
-commands = [
-    "kaggle datasets download -d trainingdatapro/fish-tracking-dataset",
-    "unzip fish-tracking-dataset.zip",
-]
+if not DATA_PATH.exists():
+    DATA_PATH.mkdir(parents=True)
 
-for command in commands:
-    subprocess.run(command, shell=True, check=True, cwd=DATA_PATH)
+if not (DATA_PATH / "fish-tracking-dataset.zip").exists():
+
+    commands = [
+        "kaggle datasets download -d trainingdatapro/fish-tracking-dataset",
+        "unzip fish-tracking-dataset.zip",
+    ]
+
+    for command in commands:
+        subprocess.run(command, shell=True, check=True, cwd=DATA_PATH)
