3c985bc6616f098b8945c6e92be14cc78619582b
yuenhern.yu@gmail.com <yuenhern.yu@gmail.com>
2024-04-11 18:07:14 +0800

    Use pytorch gpu for vision tasks

---
+++ check_gpu.py
@@ -0,0 +1,8 @@
+import torch
+
+
+if __name__ == "__main__":
+    print("CUDA is available:", torch.cuda.is_available())
+    print("CUDA device count:", torch.cuda.device_count())
+    print("CUDA current device:", torch.cuda.current_device())
+    print("CUDA device name:", torch.cuda.get_device_name())
--- download_data.py
+++ download_data.py
@@ -5,18 +5,19 @@ import subprocess
 ROOT_PATH = Path(__file__).resolve().parent
 DATA_PATH = ROOT_PATH / "data"
 
-if DATA_PATH.exists():
-    print("Data folder already exists")
-else:
-    DATA_PATH.mkdir(parents=True)
+if __name__ == "__main__":
+    if DATA_PATH.exists():
+        print("Data folder already exists")
+    else:
+        DATA_PATH.mkdir(parents=True)
 
-if (DATA_PATH / "fish-tracking-dataset.zip").exists():
-    print("Dataset has already been downloaded")
-else:
-    commands = [
-        "kaggle datasets download -d trainingdatapro/fish-tracking-dataset",
-        "unzip fish-tracking-dataset.zip",
-    ]
+    if (DATA_PATH / "fish-tracking-dataset.zip").exists():
+        print("Dataset has already been downloaded")
+    else:
+        commands = [
+            "kaggle datasets download -d trainingdatapro/fish-tracking-dataset",
+            "unzip fish-tracking-dataset.zip",
+        ]
 
-    for command in commands:
-        subprocess.run(command, shell=True, check=True, cwd=DATA_PATH)
+        for command in commands:
+            subprocess.run(command, shell=True, check=True, cwd=DATA_PATH)
